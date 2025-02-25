6e9752c0786a866e9a2d0843ec74044e99ab7159
RushingAlien <rushing27alien@gmail.com>
2025-02-25 17:59:32 +0700

    make install.sh more descriptive

--- install.sh
+++ install.sh
@@ -14,12 +14,29 @@ ROCM_INSTALL_PATH=$HOME/.local/rocm
 
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/rocm 
-# install dwarfs
-curl $DWARFS_URI -o $HOME/.local/bin/$DWARFS_BIN &&
 
-ln -s $DWARFS_BIN $HOME/.local/bin/dwarfs
-ln -s $DWARFS_BIN $HOME/.local/bin/mkdwarfs
-ln -s $DWARFS_BIN $HOME/.local/bin/dwarfsck
+# Check if $HOME/.local/bin is in PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo "$HOME/.local/bin is not in PATH. Adding it to PATH..."
+
+    # Add $HOME/.local/bin to PATH in .bash_profile
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.bash_profile
+    PATH="$HOME/.local/bin:$PATH"
+    echo "Added $HOME/.local/bin to PATH in .bash_profile"
+else
+    echo "$HOME/.local/bin is already in PATH. Skipping PATH modification."
+fi
+
+# Check if dwarfs is already installed
+if ! which dwarfs > /dev/null 2>&1; then
+  echo "dwarfs not found in PATH. Installing dwarfs..."
+  # install dwarfs
+  curl $DWARFS_URI -o $HOME/.local/bin/$DWARFS_BIN &&
+
+  ln -s $DWARFS_BIN $HOME/.local/bin/dwarfs
+  ln -s $DWARFS_BIN $HOME/.local/bin/mkdwarfs
+  ln -s $DWARFS_BIN $HOME/.local/bin/dwarfsck
+fi
 
 # download rocm bundle
 curl $URI -o $XDG_DATA_HOME/.local/rocm-portable.dwarfs &&
@@ -32,9 +49,12 @@ curl $URI -o $XDG_DATA_HOME/.local/rocm-portable.dwarfs &&
 mkdir -p $ICD_INSTALL_PATH
 
 # install rocm bundle
-
 echo "$ROCM_INSTALL_PATH/lib/libamdocl64.so" > $ICD_INSTALL_PATH/rocm-portable.icd
+echo "$ICD_INSTALL_PATH/rocm-portable.icd created"
 $HOME/.local/bin/dwarfs $HOME/.local/rocm-portable.dwarfs $ROCM_INSTALL_PATH
-echo "$HOME/.local/bin/dwarfs $HOME/.local/rocm-portable.dwarfs $ROCM_INSTALL_PATH" >> $HOME/.bash_profile
+echo "rocm-portable.dwarfs mounted to $ROCM_INSTALL_PATH"
+
+echo "dwarfs $HOME/.local/rocm-portable.dwarfs $ROCM_INSTALL_PATH" >> $HOME/.bash_profile
 
 flatpak --user override --filesystem=/sys/module/amdgpu:ro --filesystem=~/.local/share/OpenCL:ro --filesystem=~/.local/rocm:ro
+echo -e "Added the following flatpak overrides:  \n--filesystem=/sys/module/amdgpu:ro \n--filesystem=~/.local/share/OpenCL:ro \n--filesystem=~/.local/rocm:ro"  
