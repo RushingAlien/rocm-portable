c1999f16acef7562ea0e4d33bc4b6cebd9e7fc4f
RushingAlien <rushing27alien@gmail.com>
2025-02-26 17:06:35 +0700

    remove checksum file if matches

--- install.sh
+++ install.sh
@@ -72,6 +72,7 @@ download_and_compare_checksum() {
     download_and_compare_checksum
   fi
   echo "Local rocm-portable.dwarfs is up to date."
+  rm $XDG_RUNTIME_DIR/$ROCM_CHECKSUM
   return 0
 }
 
